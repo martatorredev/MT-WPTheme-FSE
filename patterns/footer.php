0f6dbe74f071414d2514cd189e4aa5452348ccbb
Marta Torre <hola@martatorre.dev>
2023-07-07 19:19:03 -0400

    feat: Semifinished footer part

---
+++ patterns/footer.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Title: footer
+ * Slug: martatorre/footer
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"},"blockGap":"0"}},"layout":{"type":"constrained","contentSize":"800px"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:columns {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
+<div class="wp-block-columns are-vertically-aligned-top" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:navigation {"ref":97,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"},"style":{"typography":{"textTransform":"none","textDecoration":"underline","fontStyle":"normal","fontWeight":"400"},"spacing":{"blockGap":"0.2rem"}},"fontSize":"medium"} /-->
+
+<!-- wp:social-links {"iconColor":"white","iconColorValue":"#ffffff","size":"has-normal-icon-size","align":"center","className":"is-style-logos-only"} -->
+<ul class="wp-block-social-links aligncenter has-normal-icon-size has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"google.com","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"google.com","service":"github"} /-->
+
+<!-- wp:social-link {"url":"google.com","service":"wordpress"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"blockGap":"var:preset|spacing|30"}}} -->
+<div class="wp-block-column is-vertically-aligned-top" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:navigation {"ref":100,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"},"style":{"spacing":{"blockGap":"0.2rem"},"typography":{"textTransform":"none","textDecoration":"underline","fontStyle":"normal","fontWeight":"400"}},"fontSize":"medium"} /-->
+
+<!-- wp:social-links {"iconColor":"white","iconColorValue":"#ffffff","size":"has-normal-icon-size","align":"center","className":"is-style-logos-only"} -->
+<ul class="wp-block-social-links aligncenter has-normal-icon-size has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"#","service":"mastodon"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:image {"align":"center","id":121,"sizeSlug":"large","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image aligncenter size-large is-style-default"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Logo-Marta-Torre-grande.svg" alt="" class="wp-image-121"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|40","right":"0","bottom":"var:preset|spacing|30","left":"0"}}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="margin-top:var(--wp--preset--spacing--40);margin-right:0;margin-bottom:var(--wp--preset--spacing--30);margin-left:0">[Aquí el texto referente a los contenidos o sus licencias]</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"small","fontFamily":"space-grotesk"} -->
+<p class="has-text-align-center has-space-grotesk-font-family has-small-font-size"><strong>Code is Poetry ❤️ by WordPress</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40","padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|50"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:image {"id":11394,"linkDestination":"custom"} -->
+<figure class="wp-block-image"><a href="https://www.creadorasdebosques.com/compensa" target="_blank" rel="noreferrer noopener"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/sello-compensa.webp" alt="<?php echo esc_attr_e( 'Sello de creadorasdebosques.com esta web compensa su emisión de CO2 por consumo energético', 'Martatorre' ); ?>" class="wp-image-11394"/></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:html -->
+<div id="wcb" class="carbonbadge wcb-d"></div>
+<p><script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js" defer=""></script></p>
+<!-- /wp:html -->
+
+<!-- wp:image {"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://api.thegreenwebfoundation.org/greencheckimage/martatorre.dev" alt="<?php echo esc_attr_e( 'This website is hosted Green - checked by thegreenwebfoundation.org', 'Martatorre' ); ?>"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:navigation {"ref":129,"overlayMenu":"never","layout":{"type":"flex","orientation":"horizontal","justifyContent":"space-between","flexWrap":"wrap"},"style":{"spacing":{"blockGap":"0"},"typography":{"textTransform":"uppercase","textDecoration":"underline","fontStyle":"normal","fontWeight":"400"}},"fontSize":"small"} /--></div>
+<!-- /wp:group -->
