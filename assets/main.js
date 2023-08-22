cffd990f7c76a257216eff873e3459779413b476
Marta Torre <hola@martatorre.dev>
2023-08-21 22:49:56 -0400

    feat: Steps logic

--- assets/main.js
+++ assets/main.js
@@ -1,29 +1,50 @@
-( function () {
-	'use strict';
+const createArrow = () => {
+  const ns = 'http://www.w3.org/2000/svg'
+  const arrow = document.createElementNS(ns, 'svg')
+  const path = document.createElementNS(ns, 'path')
 
-	// Switching to mobile: https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList/onchange
-	const isMobile = window.matchMedia( '(max-width: ' + getComputedStyle( document.body ).getPropertyValue( '--custom--media-max-width--sm' ) + ')' );
-	const navigationResponsiveContainer = document.querySelector( '.site-header .wp-block-navigation__responsive-container' );
-	isMobile.onchange = ( e ) => {
-		if ( e.matches ) {
-			// <= Mobile
-		} else {
-			// > Mobile
-			// Autoclose header nav container if modal is open and browser window gets resized.
-			if ( document.body.contains( navigationResponsiveContainer ) && navigationResponsiveContainer.classList.contains( 'is-menu-open' ) ) {
-				document.querySelector( '.site-header .wp-block-navigation__responsive-container-close' ).click();
-			}
-		}
-	}
+  // Set attributes for svg
+  arrow.setAttribute('width', '46')
+  arrow.setAttribute('height', '73')
+  arrow.setAttribute('viewBox', '0 0 46 73')
 
-	// Style password protected post form.
-	const passwordButton = document.querySelector( '.post-password-form [type="submit"]' );
-	if ( document.body.contains( passwordButton ) ) {
-		const passwordButtonWrapper = document.createElement( 'div' );
-		passwordButtonWrapper.classList.add( 'wp-block-button' );
-		passwordButton.parentNode.insertBefore( passwordButtonWrapper, passwordButton );
-		passwordButtonWrapper.appendChild( passwordButton );
-		
-		passwordButton.classList.add( 'wp-block-button__link' );
-	}
-} )();
+  // Set attributes for path
+  path.setAttribute('d', 'M20.8133 71.2765C21.9848 72.4481 23.8843 72.4481 25.0559 71.2765L44.1478 52.1846C45.3193 51.013 45.3193 49.1135 44.1478 47.942C42.9762 46.7704 41.0767 46.7704 39.9051 47.942L22.9346 64.9125L5.96401 47.942C4.79244 46.7704 2.89294 46.7704 1.72137 47.942C0.549797 49.1135 0.549797 51.013 1.72137 52.1846L20.8133 71.2765ZM19.9346 0.25V3.12105H25.9346V0.25L19.9346 0.25ZM19.9346 8.86315V14.6052H25.9346V8.86315H19.9346ZM19.9346 20.3473V26.0894H25.9346V20.3473H19.9346ZM19.9346 31.8315V37.5736H25.9346V31.8315H19.9346ZM19.9346 43.3157V49.0578H25.9346V43.3157H19.9346ZM19.9346 54.7999V60.542H25.9346V54.7999H19.9346ZM19.9346 66.2841V69.1552H25.9346V66.2841H19.9346Z')
+  path.setAttribute('fill', '#11D9ED')
+
+  arrow.classList.add('arrow')
+  arrow.appendChild(path)
+
+  return arrow
+}
+
+const createArrows = qty => {
+  const arrows = []
+
+  for (let i = 0; i < qty; i++) {
+    arrows.push(createArrow())
+  }
+
+  return arrows
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const steps = document.querySelectorAll('.steps')
+
+  if (steps) {
+    steps.forEach(step => {
+      const stepQty = step.children.length
+      const arrowsQty = stepQty - 1
+
+      const arrows = createArrows(arrowsQty)
+
+      Array.from(step.children).forEach((child, index) => {
+        const arrow = arrows[index]
+
+        if (!arrow) return
+
+        child.insertAdjacentElement('afterend', arrow)
+      })
+    })
+  }
+})
